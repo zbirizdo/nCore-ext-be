90c1b74fa54c02408f53c7e048e7ccf4f5dc5e0f
birizdo.zsolt <zsolt.birizdo@prosupport.io>
2018-11-17 16:02:33 +0100

    token check

--- api/baseCrud.js
+++ api/baseCrud.js
@@ -1,9 +1,12 @@
 ObjectID = require('mongodb').ObjectID;
 
+var token = '08841av2mg2VHUYE5APIpHik38KatgKX9wTNmyWXy5F67UUoGaN9xq7wpdqIbPdw';
+
 module.exports = function(name, db) {
 
     function createOneOrMay(req, res) {
-        if (!db) return res.send('DB init error');
+        if (req.headers.authorization !== token) return res.status(500).send('Auth error');
+        if (!db) return res.status(500).send('DB init error');
 
         var isOne = !Array.isArray(req.body);
         db.collection(name)[isOne ? 'insertOne' : 'insertMany'](req.body, function (err, result) {
@@ -13,38 +16,42 @@ module.exports = function(name, db) {
     }
     
     function readAll(req, res) {
-        if (!db) return res.send('DB init error');
+        if (req.headers.authorization !== token) return res.status(500).send('Auth error');
+        if (!db) return res.status(500).send('DB init error');
 
         db.collection(name).find({}).toArray(function(err, result){
-            if(err) return res.send('find error: ' + err);
+            if(err) return res.status(500).send('find error: ' + err);
             res.send(result);
         });
     }
     
     function readOne(req, res) {
-        if (!db) return res.send('DB init error');
+        if (req.headers.authorization !== token) return res.status(500).send('Auth error');
+        if (!db) return res.status(500).send('DB init error');
 
         db.collection(name).find(ObjectID(req.params.resId)).next(function(err, result){
-            if(err) return res.send('find error: ' + err);
+            if(err) return res.status(500).send('find error: ' + err);
             res.send(result);
         });
     }
     
     function updateOne(req, res) {
-        if (!db) return res.send('DB init error');
+        if (req.headers.authorization !== token) return res.status(500).send('Auth error');
+        if (!db) return res.status(500).send('DB init error');
 
         delete req.body._id;
         db.collection(name).updateOne({_id: ObjectID(req.params.resId)}, req.body, { upsert: true }, function(err){
             if(err) return res.send('updateOne error: ' + err);
             db.collection(name).find(ObjectID(req.params.resId)).next(function(err, result){
-                if(err) return res.send('find error: ' + err);
+                if(err) return res.status(500).send('find error: ' + err);
                 res.send(result);
             });
         });
     }
 
     function updateOrCreateMany(req, res) {
-        if (!db) return res.send('DB init error');
+        if (req.headers.authorization !== token) return res.status(500).send('Auth error');
+        if (!db) return res.status(500).send('DB init error');
 
         db.collection(name).bulkWrite(req.body.map(function(res) {
             var id = res._id;
@@ -53,17 +60,18 @@ module.exports = function(name, db) {
                 updateOne: { filter: { _id: ObjectID(id) }, update: res, upsert: true }
             };
         }), function (err, result) {
-            if(err) return res.send('bulkWrite error: ' + err);
+            if(err) return res.status(500).send('bulkWrite error: ' + err);
             res.send({result: 'OK'});
         });
 
     }
     
     function deleteOne(req, res) {
-        if (!db) return res.send('DB init error');
+        if (req.headers.authorization !== token) return res.status(500).send('Auth error');
+        if (!db) return res.status(500).send('DB init error');
 
         db.collection(name).removeOne({_id: ObjectID(req.params.resId)}, function(err, result){
-            if(err) return res.send('updateOne error: ' + err);
+            if(err) return res.status(500).send('updateOne error: ' + err);
             res.send({result: 'OK'});
         });
     }
