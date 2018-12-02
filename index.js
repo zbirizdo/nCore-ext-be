f82d4c062ad8aa0a7692d4864dec0fd6ca987d86
birizdo.zsolt <zsolt.birizdo@prosupport.io>
2018-12-02 18:23:12 +0100

    mongo config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var app             = require('express')(),
     bodyParser      = require('body-parser'),
     morgan          = require('morgan');
@@ -5,19 +6,39 @@ var app             = require('express')(),
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-Object.assign   = require('object-assign');
+Object.assign = require('object-assign');
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
+
+var db = null;
+
+var initDb = function(callback) {
+    var mongoURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/ncore-ext';
+    var mongodb = require('mongodb');
+    if (mongodb == null) return;
+
+    mongodb.connect(mongoURL, function(err, conn) {
+        if (err) return callback(err);
+
+        db = conn;
+        console.log('Connected to MongoDB');
+        callback(null);
+    });
+};
 
 app.get('/', function (req, res) {
     res.render('index.html');
 });
 
 // error handling
-app.use(function(err, req, res, next){
+app.use(function(err, req, res){
     console.error(err.stack);
     res.status(500).send('Something bad happened!');
+});
+
+initDb(function(err){
+    if(err) return console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
 const PORT = process.env.PORT || 5000;
