53a2238568347da9309b772e7e5d7943e7f18b04
birizdo.zsolt <zsolt.birizdo@prosupport.io>
2018-12-02 17:37:53 +0100

    just the minimum

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var app             = require('express')(),
+    bodyParser      = require('body-parser'),
+    morgan          = require('morgan');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+Object.assign   = require('object-assign');
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+// error handling
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Something bad happened!');
+});
+
+app.listen(5000, 'localhost');
+console.log('Server running on http://%s:%s', 'localhost', 5000);
